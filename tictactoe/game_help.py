b190515a4ea09f59f4e9f5b31c601604ee0b8c71
DanTheMain <Daniil.Morozov80@yandex.ru>
2023-09-21 10:54:31 +0300

    changes based on PR review

--- tictactoe/game_help.py
+++ tictactoe/game_help.py
@@ -1,25 +1,25 @@
 from typing import Iterable
 
 
+def _format_item(template: str, options: str | Iterable[str], sep: str) -> str:
+    return template.format(
+        f"{sep}".join(options) if isinstance(options, (tuple, list)) else options
+    )
+
+
 def game_help(
     grid: str,
-    grid_options: Iterable[str],
-    choices: set[str],
+    moves: set[str],
     quit_options: Iterable[str],
     user_mark: str,
     bot_mark: str,
 ) -> str:
-    def format_item(template: str, options: str | Iterable[str], sep: str) -> str:
-        return template.format(
-            f"{sep}".join(options) if isinstance(options, (tuple, list)) else options
-        )
 
     return "\n".join(
         [
-            format_item("{}", ("Help.", "Game grid: ", f"{grid}"), "\n"),
-            format_item("Currently valid choices: {}", choices, ", "),
-            format_item("Type {} for game grid", grid_options, " or "),
-            format_item("Type {} to quit the game", quit_options, " or "),
+            _format_item("{}", ("Help.", "Game grid: ", f"{grid}"), "\n"),
+            _format_item("Currently valid choices: {}", moves, ", "),
+            _format_item("Type {} to quit the game", quit_options, " or "),
             f"User mark: '{user_mark}', bot mark: '{bot_mark}'",
         ]
     )
